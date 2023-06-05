3af1d212b94d13a415d4003533fbbe7a687257a0
Jacob Anderson <107277162+jmanderson0119@users.noreply.github.com>
2023-06-05 13:01:21 -0400

    Update ShieldCooldown.cs
    added additional description to the functionality of the script

--- Assets/PlayerScripts/ShieldCooldown.cs
+++ Assets/PlayerScripts/ShieldCooldown.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+// this script handles the cooldown for the shield
 public class ShieldCooldown : MonoBehaviour
 {
+    // cooldown tracking + activation data
     private float currentTime = 0f;
     private float startingTime;
     [SerializeField] TMP_Text shieldCooldown;
@@ -13,7 +15,7 @@ public class ShieldCooldown : MonoBehaviour
     private float shieldTBuffer;
     bool firstShield = true;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update: set text for cooldown GUI + initialization of time marking data
     void Start()
     {
         shieldCooldown.text = "";
@@ -25,12 +27,16 @@ public class ShieldCooldown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // so long as the player can shield and wishes to deploy the ability (and cooldown has been met obviously)
         if (GameObject.Find("mainPlayer").GetComponent<PlayerStats>().CanShield() && Input.GetKeyDown(KeyCode.I) && ((Time.time >= shieldTMarker + shieldTBuffer) || (firstShield)))
         {
+            // will not apply cooldown at the beginning of the level
             if (firstShield) { firstShield = false; }
-            shieldTMarker = Time.time;
-            StartCoroutine(ShieldCooldownTimer());
+            shieldTMarker = Time.time; // mark the time!
+            StartCoroutine(ShieldCooldownTimer()); // set the timer!
         }
+        
+        // track the time left and display it next to the player
         if (shieldCooldownActive)
         {
             currentTime -= 1 * Time.deltaTime;
@@ -45,6 +51,7 @@ public class ShieldCooldown : MonoBehaviour
         }
     }
 
+    // waits until cooldown is over the empty the cooldown text and deactivate the cooldown
     IEnumerator ShieldCooldownTimer()
     {
         shieldCooldownActive = true;
